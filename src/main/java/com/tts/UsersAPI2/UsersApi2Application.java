51b7ef2cf813b65eba733aa50bb11d25ce7a4dd4
Joey Whalen <jtktwhalen@gmail.com>
2021-03-24 20:40:58 -0400

    initial commit

---
+++ src/main/java/com/tts/UsersAPI2/UsersApi2Application.java
@@ -0,0 +1,13 @@
+package com.tts.UsersAPI2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/UsersAPI2/UsersApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.UsersAPI2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
