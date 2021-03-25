aa550cdd50e950408b4f09be925909d5445691f4
Joey Whalen <jtktwhalen@gmail.com>
2021-03-25 11:08:10 -0400

    Updated UserController

--- src/main/java/com/tts/UsersAPI2/Controller/UserController.java
+++ src/main/java/com/tts/UsersAPI2/Controller/UserController.java
@@ -14,7 +14,7 @@ public class UserController {
   @Autowired
   private UserRepository userRepository;
 
-  @GetMapping("/user")
+  @GetMapping("/users")
   public List<User> getUsers(@RequestParam(value = "state", required = false) String state) {
     if (state != null) {
       return (List<User>) userRepository.findByState(state);
@@ -22,22 +22,22 @@ public class UserController {
     return (List<User>) userRepository.findAll();
   }
 
-  @GetMapping("/user/{id}")
+  @GetMapping("/users/{id}")
   public Optional<User> getUserById(@PathVariable(value = "id") Long id) {
     return userRepository.findById(id);
   }
 
-  @PostMapping("/user")
+  @PostMapping("/users")
   public void createUser(@RequestBody User user) {
     userRepository.save(user);
   }
 
-  @PutMapping("/user/{id}")
+  @PutMapping("/users/{id}")
   public void createUser(@PathVariable(value = "id") Long id, @RequestBody User user) {
     userRepository.save(user);
   }
 
-  @DeleteMapping("/user/{id}")
+  @DeleteMapping("/users/{id}")
   public void createUser(@PathVariable(value = "id") Long id) {
     userRepository.deleteById(id);
   }
