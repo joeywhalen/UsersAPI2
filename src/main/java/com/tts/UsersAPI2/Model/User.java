beda1c6658e9c59fca29a9f1493ba92c61203f87
Joey Whalen <jtktwhalen@gmail.com>
2021-03-25 11:32:16 -0400

    Updated pom file for validation dependency, updated User for @Length constraints

--- src/main/java/com/tts/UsersAPI2/Model/User.java
+++ src/main/java/com/tts/UsersAPI2/Model/User.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import org.hibernate.validator.constraints.Length;
 import javax.persistence.*;
 
 @Data
@@ -16,49 +16,56 @@ public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private String id;
+
+  @Length(max = 20, message = "Your first name cannot have more than 20 characters")
   private String firstName;
+
+  @Length(min = 2, message = "Your last name must have at least 2 characters")
   private String lastName;
+
+  @Length(min = 4, message = "Your state must have at least 4 characters")
+  @Length(max = 20, message = "Your state cannot have more than 20 characters")
   private String state;
 
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public String getState() {
-    return state;
-  }
-
-  public void setState(String state) {
-    this.state = state;
-  }
-
-  @Override
-  public String toString() {
-    return "User{" +
-            "id='" + id + '\'' +
-            ", firstName='" + firstName + '\'' +
-            ", lastName='" + lastName + '\'' +
-            ", state='" + state + '\'' +
-            '}';
-  }
+//  public String getId() {
+//    return id;
+//  }
+//
+//  public void setId(String id) {
+//    this.id = id;
+//  }
+//
+//  public String getFirstName() {
+//    return firstName;
+//  }
+//
+//  public void setFirstName(String firstName) {
+//    this.firstName = firstName;
+//  }
+//
+//  public String getLastName() {
+//    return lastName;
+//  }
+//
+//  public void setLastName(String lastName) {
+//    this.lastName = lastName;
+//  }
+//
+//  public String getState() {
+//    return state;
+//  }
+//
+//  public void setState(String state) {
+//    this.state = state;
+//  }
+//
+//  @Override
+//  public String toString() {
+//    return "User{" +
+//            "id='" + id + '\'' +
+//            ", firstName='" + firstName + '\'' +
+//            ", lastName='" + lastName + '\'' +
+//            ", state='" + state + '\'' +
+//            '}';
+//  }
 }
