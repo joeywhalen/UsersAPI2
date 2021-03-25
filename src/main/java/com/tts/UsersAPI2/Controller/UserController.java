16c92e38b33287aa07cab4e3499d63b4f14a53ea
Joey Whalen <jtktwhalen@gmail.com>
2021-03-25 17:01:23 -0400

    Updating UserController for Lab Part 2

--- src/main/java/com/tts/UsersAPI2/Controller/UserController.java
+++ src/main/java/com/tts/UsersAPI2/Controller/UserController.java
@@ -3,8 +3,13 @@ package com.tts.UsersAPI2.Controller;
 import com.tts.UsersAPI2.Model.User;
 import com.tts.UsersAPI2.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,8 +33,12 @@ public class UserController {
   }
 
   @PostMapping("/users")
-  public void createUser(@RequestBody User user) {
+  public ResponseEntity<Void> createUser(@RequestBody @Valid User user, BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
     userRepository.save(user);
+    return new ResponseEntity<>(HttpStatus.CREATED);
   }
 
   @PutMapping("/users/{id}")
