f0a02216000ea181d62fa372b60723014af2533a
Joey Whalen <jtktwhalen@gmail.com>
2021-03-26 12:47:05 -0400

    Update User to remove unnecessary code

--- src/main/java/com/tts/UsersAPI2/Model/User.java
+++ src/main/java/com/tts/UsersAPI2/Model/User.java
@@ -27,45 +27,4 @@ public class User {
   @Length(max = 20, message = "Your state cannot have more than 20 characters")
   private String state;
 
-//  public String getId() {
-//    return id;
-//  }
-//
-//  public void setId(String id) {
-//    this.id = id;
-//  }
-//
-//  public String getFirstName() {
-//    return firstName;
-//  }
-//
-//  public void setFirstName(String firstName) {
-//    this.firstName = firstName;
-//  }
-//
-//  public String getLastName() {
-//    return lastName;
-//  }
-//
-//  public void setLastName(String lastName) {
-//    this.lastName = lastName;
-//  }
-//
-//  public String getState() {
-//    return state;
-//  }
-//
-//  public void setState(String state) {
-//    this.state = state;
-//  }
-//
-//  @Override
-//  public String toString() {
-//    return "User{" +
-//            "id='" + id + '\'' +
-//            ", firstName='" + firstName + '\'' +
-//            ", lastName='" + lastName + '\'' +
-//            ", state='" + state + '\'' +
-//            '}';
-//  }
 }
